c3f179ac2de5aa24ec89ab68d0fd08120e823e96
Heather Stevens <heatherveva@gmail.com>
2022-01-18 18:28:56 -0600

    hw6 start

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Global Variables
+
+// Functions
+
+// EVENT LISTENERS
+// On page load, show any past cities searched (local storage, does not erase search when website is left)
+// Search for city button click
+// Click on city to show weather

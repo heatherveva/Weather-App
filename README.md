33889b259cd60ec6484f4b3d6dd4ac2edad75fb7
Heather Stevens <heatherveva@gmail.com>
2022-01-28 16:15:28 -0600

    fetch added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# hw6
+# Homework 6 - Weather Predictor
+
+## Description
+
+This weather application was built using HTML, CSS, Javascript, and Bootstrap.
+
+When the user
+
+## Screenshot
+
+## Links to Application
+
+- Here is a link to the repository:
+- Here is the page:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 // Global Variables
 
 // Functions
+//fetch function - retrieve data from API
+const fetchUrl = `http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=4b9f7dc3f8536150bc0eb915e8e4a81b`;
+
+//fetch back tattoo
+fetch(fetchUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data.results);
+    displayResults(data.results);
+  });
+
+function displayResults() {}
 
 // EVENT LISTENERS
 // On page load, show any past cities searched (local storage, does not erase search when website is left)
 // Search for city button click
+document.querySelector("#searchForm").addEventListener("submit", handleResults);
 // Click on city to show weather
